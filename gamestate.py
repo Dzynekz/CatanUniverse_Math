0eac1e0c813d48e9cef44e6f31f86f2b1bf65c2d
Dzynekz <marcinzwonik01@gmail.com>
2024-06-26 17:00:39 +0200

    Simple resource and knights management

--- gamestate.py
+++ gamestate.py
@@ -17,6 +17,9 @@ class GameState():
     
     def get_player_with_longest_road(self) -> Player:
         return self.player_with_longest_road 
+    
+    def get_player_with_most_knights(self) -> Player:
+        return self.player_with_most_knights
 
     def player_with_longest_roads_update(self, player: Player) -> None:        
         if self.player_with_longest_road is None or player.get_buildings('roads') > self.player_with_longest_road.get_buildings('roads'):
@@ -26,8 +29,13 @@ class GameState():
                 old_player.points -= 2
             player.points += 2
 
-    def player_with_most_knights_update(self) -> Player:
-        pass
+    def player_with_most_knights_update(self, player: Player) -> None:
+        if self.player_with_most_knights is None or player.get_knights() > self.player_with_most_knights.get_knights():
+            old_player = self.player_with_most_knights
+            self.player_with_most_knights = player
+            if old_player:
+                old_player.points -= 2
+            player.points += 2
 
 
 
--- player.py
+++ player.py
@@ -1,5 +1,5 @@
 from buildings import Building, Village, City, Road
-
+from cards import Card, Knight, VictoryPoint, Monopoly, YearOfPlenty, RoadBuilding
 class Player():
     MAX_VILLAGES = 5
     MAX_CITIES = 4
@@ -16,30 +16,77 @@ class Player():
             'roads': 0
         }
         self.resources = {'wood': 0, 'brick': 0, 'sheep': 0, 'grain': 0, 'ore': 0}
+        self.cards = []
 
     def get_name(self) -> str:
         return self.name
     
-    def add_building(self, building: Building, game_state) -> None:
-        if isinstance(building, Village):
-            if self.buildings['villages'] < self.MAX_VILLAGES:
-                self.buildings['villages'] += 1
-                self.points += 1
-        elif isinstance(building, City):
-            if self.buildings['cities'] < self.MAX_CITIES and self.buildings['villages'] > 0:
-                self.buildings['cities'] += 1
-                self.buildings['villages'] -= 1
-                self.points += 1
-        elif isinstance(building, Road):    
-            self.buildings['roads'] += 1
-            game_state.player_with_longest_roads_update(self)
-
+    def get_points(self) -> int:
+        return self.points
     
     def get_buildings(self, building) -> int:
         return self.buildings[building]
     
-    def get_recources(self, recourse) -> int:
-        return self.resources[recourse]
+    def get_recources(self) -> int:
+        return self.resources
+    
+    def get_knights(self) -> int:
+        return self.knights
+    
+    def add_resources(self, resource, quantity):
+        '''Not sure if resources shouldn't be a different class'''
+        if resource in self.resources:
+            self.resources[resource] += quantity
+
+    
+    def use_resources(self, resources_needed: dict) -> bool:
+        for key, value in resources_needed.items():
+            if self.resources[key] < value:              
+                return False
+        for key, value in self.resources.items():
+            if key in resources_needed:
+                self.resources[key] -= resources_needed[key]
+        return True
+            
+
+    
+    def add_building(self, building: Building, game_state) -> None:
+        if isinstance(building, Village):
+            if self.use_resources(Village.COST):
+                if self.buildings['villages'] < self.MAX_VILLAGES:
+                    self.buildings['villages'] += 1
+                    self.points += 1
+            else:
+                print('Nie wystarczające zasoby')
+        elif isinstance(building, City):
+            if self.use_resources(City.COST):
+                if self.buildings['cities'] < self.MAX_CITIES and self.buildings['villages'] > 0:
+                    self.buildings['cities'] += 1
+                    self.buildings['villages'] -= 1
+                    self.points += 1
+                elif self.buildings['villages'] == 0:
+                    print('You dont have a village, cant built a city')
+            else:
+                print('Nie wystarczające zasoby')
+        elif isinstance(building, Road):    
+            if self.use_resources(Road.COST):
+                self.buildings['roads'] += 1
+                game_state.player_with_longest_roads_update(self)
+            else:
+                print('Nie wystarczające zasoby')
+    
+    #def add_card(self, game_state) -> None:
+
+
+    def use_card(self, card: Card, game_state) -> None:
+        if card in self.cards:
+            if isinstance(card,Knight):
+                self.knights += 1
+                game_state.player_with_most_knights_update(self)
+            # Rest of if's
+
+    
+    
     
 
                 
