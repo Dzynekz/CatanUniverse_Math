83b666dd54b305eecb43fd81a0906c58685473c2
Dzynekz <marcinzwonik01@gmail.com>
2024-06-27 12:41:26 +0200

    Added Importance of resources

--- strategy.py
+++ strategy.py
@@ -1,5 +1,6 @@
 import itertools
 import math
+import numpy as np
 import pandas as pd
 from deck import Deck
 
@@ -33,12 +34,8 @@ def combination_of_possible_wins():
     }
 
     combinations = list(itertools.product(*possible_combinations.values()))
-
-    # Konwersja do DataFrame
     combinations_all = pd.DataFrame(combinations, columns=possible_combinations.keys())
-
     combinations_all['points'] = combinations_all.apply(lambda row: row['village'] + row['city'] * 2 + row['mostRoads'] * 2 + row['mostPorts'] * 2 + row['mostKnights'] * 2 + row['victoryPointCards'], axis=1)
-
     possible_wins = combinations_all[combinations_all['points'].isin([15, 16])].reset_index()
 
     return possible_wins
@@ -50,8 +47,8 @@ def add_resource_cost_of_wins(df, deck: Deck):
         - dont know how to calculate roads needed per city - for now its 8 roads always **
         - dont know how to do ports I assume that mostPorts means you have to add 4 another roads
         - substract -4, -2 or 0 because of first 2 villages and roads'''
-    df['wood'] = df['village'] + df['city'] + max(df['mostRoads'] * 12, 8) + df['mostPorts'] * 4 - 4
-    df['brick'] = df['village'] + df['city'] + max(df['mostRoads'] * 12, 8) + df['mostPorts'] * 4 - 4
+    df['wood'] = df['village'] + df['city'] + np.maximum(df['mostRoads'] * 12, 8) + df['mostPorts'] * 4 - 4
+    df['brick'] = df['village'] + df['city'] + np.maximum(df['mostRoads'] * 12, 8) + df['mostPorts'] * 4 - 4
     df['sheep'] = df['village'] + df['city'] + df['mostKnights'] * knights_probability(deck) + df['victoryPointCards'] * vp_probability(deck) -2
     df['grain'] = df['village'] + df['city'] * 3 + df['mostKnights'] * knights_probability(deck) +  df['victoryPointCards'] * vp_probability(deck) -2
     df['ore'] = df['city'] * 3 + df['mostKnights'] * knights_probability(deck) +  df['victoryPointCards'] * vp_probability(deck)
@@ -92,5 +89,40 @@ def resources_drop_chance(board: dict) -> dict:
         resource_probability[resource] = sum(probabilities[number] for number in numbers)
     return resource_probability
 
-def calculate_rounds_needed(df, resource_probabilities):
-    pass
+def calculate_rounds_needed(df, resource_probabilities: dict):
+    '''It needs to calculate not only on drop probability, but also ??? on availability of trading with bank (4:1) and other players.'''
+    rounds_needed = []
+    for _, row in df.iterrows():
+        rounds_for_resource = []
+        for resource, probability in resource_probabilities.items():
+            rounds_for_resource.append(row[resource] / probability)
+        rounds_needed.append(math.ceil(max(rounds_for_resource)))
+    df['rounds_needed'] = rounds_needed
+    return df
+
+def select_top_percent_possible_wins(df):
+    treshold = df['rounds_needed'].quantile(0.1)
+    top10 = df[df['rounds_needed'] <= treshold]
+    return top10
+
+def calculate_importance_of_resources(df, drop_chance: dict):
+    '''Dont know if its working how it should'''
+    total_drop_chance = sum(drop_chance.values())
+    resources_used = {'wood': 0, 'brick': 0, 'sheep': 0, 'grain': 0, 'ore': 0, 'used_resources':0}
+    resources_importance = {}
+    df = select_top_percent_possible_wins(df)
+
+    for _, row in df.iterrows():
+        for resource in resources_used.keys():
+            resources_used[resource] += row[resource]
+    
+    for resource in resources_used.keys():
+        if resource != 'used_resources':
+            # Here is a line that I am not sure about
+            resources_importance[resource] = (resources_used[resource] / resources_used['used_resources']) * (drop_chance[resource] / total_drop_chance)
+    
+    return resources_importance
+    
+
+    
+
